2aae190e4dfc3fd320af1d349264b66ae9c0a2f3
jakith <jakith100@gmail.com>
2020-05-22 04:02:50 -0300

    Updates

---
+++ precache-manifest.9dd2fc80fef5aae8e5b203e3df96ac05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef67b14af286cd86445dea3d463baef3",
+    "url": "/Trip_app/index.html"
+  },
+  {
+    "revision": "cd22ce6c68880bc43c44",
+    "url": "/Trip_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d6f995c3366a98c1c66a",
+    "url": "/Trip_app/static/js/2.f42a383b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Trip_app/static/js/2.f42a383b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd22ce6c68880bc43c44",
+    "url": "/Trip_app/static/js/main.985835c6.chunk.js"
+  },
+  {
+    "revision": "425e91d7d3d160ca067a",
+    "url": "/Trip_app/static/js/runtime-main.3bf0c4d3.js"
+  }
+]);
